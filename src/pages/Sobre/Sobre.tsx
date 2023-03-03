1da8d5bca5e845a732f7493610b5f9b4f86677ed
jardel <jardelcruz333@gmail.com>
2023-03-03 04:52:04 -0300

    finalizando a pagina sobre

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -1,36 +1,43 @@
 import style from "./Sobre.module.scss";
 import styleTema from "@/assets/style/Tema.module.scss";
-import casa from "@/assets/images/casa.png"
-import massa1 from "@/assets/images/massa1.png"
-import massa2 from "@/assets/images/massa2.png"
+import casa from "@/assets/images/casa.png";
+import massa1 from "@/assets/images/massa1.png";
+import massa2 from "@/assets/images/massa2.png";
 
-const imagens = [massa1, massa2];
+const IMAGENS = [massa1, massa2];
 
 export default function Sobre() {
   return (
     <section>
       <h3 className={styleTema.titulo}>Sobre</h3>
       <div className={style.sobreNos}>
-        <img src={casa} alt="Casa alurone"/>
+        <img src={casa} alt="Casa alurone" />
         <div className={style.sobreNos__texto}>
           <p>
-            Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos pelos ingredientes tradicionais da culinária Italiana, frescos e de excelente qualidade para que sua experiência seja ainda mais intensa!
+            Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa
+            Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos
+            pelos ingredientes tradicionais da culinária Italiana, frescos e de
+            excelente qualidade para que sua experiência seja ainda mais
+            intensa!
           </p>
           <p>
-            Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto!
+            Também possuímos uma cardápio de carnes com muitas opções de acordo
+            com o seu gosto!
           </p>
           <p>
-            Para acompanhar as massas italianas, Aluroni possui uma reserva de vinhos especiais, que harmonizam perfeitamente com o seu parto, seja carne ou massa!
+            Para acompanhar as massas italianas, Aluroni possui uma reserva de
+            vinhos especiais, que harmonizam perfeitamente com o seu parto, seja
+            carne ou massa!
           </p>
         </div>
       </div>
       <div className={style.imagens}>
-        {imagens.map((imagem, index) => (
+        {IMAGENS.map((imagem, index) => (
           <div key={index} className={style.imagens__imagem}>
             <img src={imagem} alt="Imagem de massa" />
           </div>
         ))}
       </div>
     </section>
-  )
+  );
 }
